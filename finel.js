8ddf76d0cc40ad4716a693705019eee1ab084e97
shahneouj <shahneouj@gmail.com>
2025-08-13 22:59:43 +0600

    forth problem solved

--- finel.js
+++ finel.js
@@ -30,3 +30,22 @@ function bestTeam(player1, player2) {
     return player2.name;
   }
 }
+
+function isSame(arr1, arr2) {
+  let match = 0;
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return "Invalid"
+  } else if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      match++;
+    }
+  }
+  if (match == arr1.length) {
+    return true
+  } else {
+    return false
+  };
+}
---
+++ problem4.js
@@ -0,0 +1,19 @@
+function isSame(arr1, arr2) {
+  let match = 0;
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return "Invalid"
+  } else if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      match++;
+    }
+  }
+  if (match == arr1.length) {
+    return true
+  } else {
+    return false
+  };
+}
+isSame({ data: [2, 5, 6] }, [2, 5, 6]);
